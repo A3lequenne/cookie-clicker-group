03aa62defe2ba674439e279fb9e495460c4d3f7d
konstantiinov <ravehsingh@gmail.com>
2023-10-24 11:14:21 +0200

    added the score

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (() => {
-  let score = document.getElementById("pointsh2");
+  let scoreElement = document.getElementById("pointsh2");
   let multi = document.getElementById("multi");
   let bonus = document.getElementById("bonus");
   let auto = document.getElementById("auto");
@@ -8,5 +8,23 @@
   let info = document.getElementById("info");
   let pricebonus = document.getElementById("pricebonus");
   let priceauto = document.getElementById("priceauto");
-  let rocket = document.getElementById('rocket');
+  let rocket = document.getElementById("rocket");
+
+  let score = 0;
+  function updateScore() {
+    scoreElement.textContent = score;
+  }
+
+  rocket.addEventListener("click", function () {
+    score++;
+    updateScore();
+
+    if (score >= purchaseCost) {
+      score -= purchaseCost;
+      score = Math.max(0, score);
+      updateScore();
+    }
+  });
+  updateScore();
+  return score;
 })();
